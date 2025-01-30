0a5ab061434d7b9f9e3978b67ea0435df4184a74
ledaj <eljadaoui.remi@gmail.com>
2025-01-30 23:02:12 +0100

    update 241230_TicTacToe_V1.py

--- 241230_TicTacToe_V1.py
+++ 241230_TicTacToe_V1.py
@@ -5,28 +5,31 @@ import logging
 board = ["-" for x in range(9)]
 player = 'X'
 is_WinState = False
-is_DrawState = False
+is_GameOver = False
 
-def print_board(): # créer les neufs cases
+def printBoard(): 
+  # POUR créer le tableau à 9 cases je crée troiw lignes de trois cases
 
     row1 = "|{}|{}|{}|".format(board[0], board[1], board[2])
     row2 = "|{}|{}|{}|".format(board[3], board[4], board[5])
     row3 = "|{}|{}|{}|".format(board[6], board[7], board[8])
     
-    #impression du tableau
+    #POUR afficher le tableau je l'imprime avec du vide autour
     print()
     print(row1)
     print(row2)
     print(row3) 
     print() 
 
-def swapPlayer(player) : #changer de joueur
+def swapPlayer(player) : 
+  # POUR changer de joueur j'examine la valeur du player
     return 'X' if player == 'O' else 'O'
 
-def playerMove(player): # Tour de jeu
+def playerMove(player): 
+  # POUR jouer son tour, j'affiche le tableau, je recueille le choix du joueur et je le remplace dans le tableau
     print("Your turn, player {}.".format(player))
     print()
-    print_board()
+    printBoard()
     print()
     while True:
         try:
@@ -54,12 +57,12 @@ def playerMove(player): # Tour de jeu
             print("Try again player {}.".format(player))
             print() 
 
-def gameStart(player, board): # sert à relancer une partie
-    # tirage du premier joueur
+def gameStart(player, board): 
+  #POUR lancer la partie, je tire le premier joueur
     randomNumber = random.randint(0, 1)
     player = 'X' if randomNumber == 1 else 'O'
     
-    #reset de toutes les cases
+    #et je vide le tableau
     while "X" in board:
             for X in range(0,8):
                 board[X] = "-"
@@ -69,22 +72,22 @@ def gameStart(player, board): # sert à relancer une partie
     gameLoop(player)
 
 
-def is_WinState(player): # Conditions de victoire
-        # ligne entière ?
+def is_WinState(player): # POUR déteminer la victoire: 
+        # je vérifie les lignes entières ?
         if board[0] == board[1] == board[2] == player:
             return True
         elif board[3] == board[4] == board[5] == player:
             return True
         elif board[6] == board[7] == board[8] == player:
             return True
-        # colonne entière ?
+        # les colonnes entières ?
         elif board[0] == board[3] == board[6] == player:
             return True
         elif board[1] == board[4] == board[7] == player:
             return True
         elif board[2] == board[5] == board[8] == player:
             return True
-        # diagonale entière ?
+        # les diagonales entières ?
         elif board[0] == board[4] == board[8] == player:
             return True
         elif board[2] == board[4] == board[6] == player:
@@ -92,14 +95,14 @@ def is_WinState(player): # Conditions de victoire
         else:
             return False
 
-def is_DrawState(board): # vérifie si toutes les cases sont pleines
+def is_GameOver(board): # POUR vérifier la fin de partie, je vérifie si toutes les cases sont pleines
     return True if "-" not in board else False
     
 def gameLoop(player): # Boucle de jeu
     while True:
         if is_WinState("X") == True: # victoire atteinte pour X ?
             print()
-            print_board()
+            printBoard()
             print()
             print("Game Over.")
             print("Player X WINS !")
@@ -107,13 +110,13 @@ def gameLoop(player): # Boucle de jeu
             break
         elif is_WinState("O") == True: # victoire atteinte pour O ?
             print()
-            print_board()
+            printBoard()
             print()
             print("Game Over.")
             print ("Player O WINS !")
             newGame(player,board)
             break
-        elif is_DrawState(board) == True: # le tableau est plein ?
+        elif is_GameOver(board) == True: # le tableau est plein ?
             print("Game Over.")
             print ("Nobody wins.")
             newGame(player,board)
@@ -122,7 +125,7 @@ def gameLoop(player): # Boucle de jeu
             playerMove(player)
             player = swapPlayer(player)
     
-def newGame(player,board): # Choix d'une nouvelle partie
+def newGame(player,board): # POUR relancer une nouvelle partie, je demande au joueur et lance gameStart ou break en fonction
     print("Try again ?")
     print()
     while True:
