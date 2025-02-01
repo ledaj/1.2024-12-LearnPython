bf4fc6048fa62d06e425c7e1de56e065dfce9ed6
ledaj <eljadaoui.remi@gmail.com>
2025-02-01 22:32:22 +0100

    update 250102_Timer.py

--- 250102_Timer.py
+++ 250102_Timer.py
@@ -1,7 +1,30 @@
 import logging
+import clock
 
 # Variables de base
 
 # blocks
+
+## TO know time I ask the clock what time it is
+
+## TO set the countdown I ask the delay
+
+## TO set the deadline I add the delay to the actual time
+
+## TO run the countdown I ask for a go
+
+## TO obtain the remaining time I substrack the time to the deadline
+
+## TO display the countdown I print the remaining time
+
+## TO check if countdown over I compare the deadline and the actual time
+
+## TO stop the countdown I stop the loop
+
+## TO signal countdown over I print the alarm
+
+## TO know if start over I ask for input
+
+## TO start over I clean the variables and launch again
 
 # main init
