0a732056810d4d29354b81467eb153b5d18f39da
ledaj <eljadaoui.remi@gmail.com>
2025-02-08 02:01:07 +0100

    Create 250208_binarySearch.py

---
+++ 250208_binarySearch.py
@@ -0,0 +1,19 @@
+import random
+number = random.randint(1, 10)
+
+def binarySearch(number, list):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == number:
+            return mid
+        if guess > number:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+print(binarySearch(number, [1, 3, 5, 7, 9]))
+print(number)
